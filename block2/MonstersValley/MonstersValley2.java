b3ccdb5415166cf40e49bade59c82abc6564d316
JasonZhao5 <jasonjzjc@gmail.com>
2024-03-19 14:18:58 +1030

    MonstersValley2

--- block2/MonstersValley/MonstersValley2.java
+++ block2/MonstersValley/MonstersValley2.java
@@ -4,23 +4,22 @@ public class MonstersValley2 {
 
         int i = 1;
 
-        return getMin(dread, price,dread[0], price[0], i);
+        return getMin(dread, price, dread[0], price[0], i);
 
     }
 
-    public static int getMin(int[] dread, int[] price, int d, int p, int i){
+    public static int getMin(int[] dread, int[] price, int d, int p, int i) {
 
         if (i == dread.length) {
             return p;
         }
         if (dread[i] > d) {
-            return getMin(dread, price, d+dread[i],p+price[i], i+1);
+            return getMin(dread, price, d + dread[i], p + price[i], i + 1);
         } else {
-            int x = getMin(dread, price, d+dread[i],p+price[i], i + 1);
+            int x = getMin(dread, price, d + dread[i], p + price[i], i + 1);
             int y = getMin(dread, price, d, p, i + 1);
             return Math.min(x, y);
         }
-
 
 
     }
